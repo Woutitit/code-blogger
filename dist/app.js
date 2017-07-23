362001db1844369fa0b459b8f99d7d3d72b7e872
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-07-23 22:14:13 +0200

    Add font stylings

--- dist/app.js
+++ dist/app.js
@@ -63,90 +63,16 @@
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports) {
+/******/ ({
 
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
-// css base code, injected by the css-loader
-module.exports = function(useSourceMap) {
-	var list = [];
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
 
-	// return the list of modules as css string
-	list.toString = function toString() {
-		return this.map(function (item) {
-			var content = cssWithMappingToString(item, useSourceMap);
-			if(item[2]) {
-				return "@media " + item[2] + "{" + content + "}";
-			} else {
-				return content;
-			}
-		}).join("");
-	};
-
-	// import a list of modules into the list
-	list.i = function(modules, mediaQuery) {
-		if(typeof modules === "string")
-			modules = [[null, modules, ""]];
-		var alreadyImportedModules = {};
-		for(var i = 0; i < this.length; i++) {
-			var id = this[i][0];
-			if(typeof id === "number")
-				alreadyImportedModules[id] = true;
-		}
-		for(i = 0; i < modules.length; i++) {
-			var item = modules[i];
-			// skip already imported module
-			// this implementation is not 100% perfect for weird media query combinations
-			//  when a module is imported multiple times with different media queries.
-			//  I hope this will never occur (Hey this way we have smaller bundles)
-			if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
-				if(mediaQuery && !item[2]) {
-					item[2] = mediaQuery;
-				} else if(mediaQuery) {
-					item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
-				}
-				list.push(item);
-			}
-		}
-	};
-	return list;
-};
-
-function cssWithMappingToString(item, useSourceMap) {
-	var content = item[1] || '';
-	var cssMapping = item[3];
-	if (!cssMapping) {
-		return content;
-	}
-
-	if (useSourceMap && typeof btoa === 'function') {
-		var sourceMapping = toComment(cssMapping);
-		var sourceURLs = cssMapping.sources.map(function (source) {
-			return '/*# sourceURL=' + cssMapping.sourceRoot + source + ' */'
-		});
-
-		return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
-	}
-
-	return [content].join('\n');
-}
-
-// Adapted from convert-source-map (MIT)
-function toComment(sourceMap) {
-	// eslint-disable-next-line no-undef
-	var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
-	var data = 'sourceMappingURL=data:application/json;charset=utf-8;base64,' + base64;
-
-	return '/*# ' + data + ' */';
-}
-
+__webpack_require__(1);
 
 /***/ }),
-/* 1 */
+
+/***/ 1:
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(2);
@@ -154,110 +80,26 @@ __webpack_require__(22);
 __webpack_require__(3);
 
 /***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(0)(undefined);
-// imports
+/***/ 2:
+/***/ (function(module, exports) {
 
-
-// module
-exports.push([module.i, "* {\r\n\tbox-sizing: border-box;\r\n}\r\n\r\nhtml {\r\n\tfont-size: 62.5%;\r\n}\r\n\r\nbody {\r\n\tfont-size: 1.4rem;\r\n\tcolor: #4A4A4A;\r\n}\r\n\r\nh3 {\r\n\tfont-family: \"Merriweather\", serif;\r\n}\r\n\r\nh3 {\r\n\tcolor: #000;\r\n\tfont-size: 2.1rem;\r\n\tmargin: 1em 0 0;\r\n}\r\n\r\nh3 > a {\r\n\tcolor: inherit;\r\n\ttext-decoration: none;\r\n}\r\n\r\nh3:first-child {\r\n\tmargin-top: 0;\r\n}\r\n\r\np {\r\n\tline-height: 1.75;\r\n}\r\n\r\na {\r\n\ttransition: .3s color;\r\n}\r\n\r\na:hover {\r\n\tcolor: #0a91bc;\r\n}", ""]);
-
-// exports
-
+// removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(0)(undefined);
-// imports
-exports.i(__webpack_require__(4), "");
-exports.i(__webpack_require__(5), "");
-exports.i(__webpack_require__(6), "");
+/***/ 22:
+/***/ (function(module, exports) {
 
-// module
-exports.push([module.i, "/* Card */\r\n\r\n/* Masonry */\r\n\r\n/* Page */", ""]);
-
-// exports
-
+// removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 4 */
-/***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(0)(undefined);
-// imports
-
-
-// module
-exports.push([module.i, ".card {\r\n\tbackground-color: #fff;\r\n\tborder: 1px solid rgba(0,0,0,.125);\r\n\tborder-radius: .25rem;\r\n\tdisplay: flex;\r\n\tflex-direction: column;\r\n\tposition: relative;\r\n}\r\n\r\n.card__image img {\r\n\twidth: 276px;\r\n\theight: auto;\r\n}\r\n\r\n.card__block {\r\n\tpadding: 1.6rem;\r\n}\r\n\r\n.card__footer {\r\n\tpadding: .8rem 1.6rem;\r\n}", ""]);
-
-// exports
-
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(0)(undefined);
-// imports
-
-
-// module
-exports.push([module.i, ".masonry-layout .card {\r\n\tmargin-bottom: 1.6rem;\r\n}", ""]);
-
-// exports
-
-
-/***/ }),
-/* 6 */
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(0)(undefined);
-// imports
-
-
-// module
-exports.push([module.i, ".page--front {\r\n\tbackground-color: #f5f5f5;\r\n}", ""]);
-
-// exports
-
-
-/***/ }),
-/* 7 */,
-/* 8 */,
-/* 9 */,
-/* 10 */,
-/* 11 */,
-/* 12 */,
-/* 13 */,
-/* 14 */
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(0)(undefined);
-// imports
-
-
-// module
-exports.push([module.i, "/* Layout */\r\n.layout-content {\r\n\tmargin: 0 auto;\r\n\tpadding: 0 16px;\r\n\tmax-width: 960px;\r\n}\r\n\r\n/* Grid */\r\n.row {\r\n\tdisplay: flex;\r\n\tflex-wrap: wrap;\r\n}\r\n\r\n.row {\r\n    margin-right: -16px;\r\n    margin-left: -16px;\r\n}\r\n\r\n.col-md-2,\r\n.col-md-4,\r\n.col-md-6,\r\n.col-md-10 {\r\n\twidth: 100%;\r\n}\r\n\r\n@media (min-width: 992px) {\r\n\t.col-md-2 {\r\n\t\tflex: 0 0 16.666667%;\r\n\t}\r\n\r\n\t.col-md-4 {\r\n\t\tflex: 0 0 33.333334%;\r\n\t\tmax-width: 33.333334%;\r\n\t}\r\n\r\n\t.col-md-6 {\r\n\t\tflex: 0 0 50%;\r\n\t\tmax-width: 50%;\r\n\t}\r\n\r\n\t.col-md-10 {\r\n\t\tflex: 0 0 83.333333%;\r\n\t}\r\n\r\n\t.col-md-offset-2 {\r\n\t\tmargin-left: 16.666667%;\r\n\t}\r\n}\r\n\r\n@media (min-width: 992px) {\r\n\t.col-md-2,\r\n\t.col-md-4,\r\n\t.col-md-6,\r\n\t.col-md-10 {\r\n\t\tpadding-right: 16px !important;\r\n\t\tpadding-left: 16px !important;\r\n\t}\r\n}\r\n\r\n", ""]);
-
-// exports
-
-
-/***/ }),
-/* 15 */,
-/* 16 */,
-/* 17 */,
-/* 18 */,
-/* 19 */,
-/* 20 */,
-/* 21 */,
-/* 22 */
+/***/ 3:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ })
-/******/ ]);
+
+/******/ });
